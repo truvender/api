7f0ec10846f91845e913d06895ba32feb89788be
zong <zong-dev@protonmail.com>
2022-01-13 21:45:53 +0100

    Wallet: Bind repository to interface

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -4,10 +4,14 @@ namespace App\Providers;
 
 use App\Interfaces\{
     AuthInterface,
+    WalletInterface,
     DashboardInterface,
 };
-use App\Repositories\AuthRepository;
-use App\Repositories\DashboardRepository;
+use App\Repositories\{
+    AuthRepository,
+    WalletRepository,
+    DashboardRepository,
+};
 use Illuminate\Support\ServiceProvider;
 
 class RepositoryServiceProvider extends ServiceProvider
@@ -31,5 +35,6 @@ class RepositoryServiceProvider extends ServiceProvider
     {
         $this->app->bind(AuthInterface::class, AuthRepository::class );
         $this->app->bind(DashboardInterface::class, DashboardRepository::class);
+        $this->app->bind(WalletInterface::class, WalletRepository::class);
     }
 }
