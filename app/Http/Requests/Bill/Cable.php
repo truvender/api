81da057e314cdd172ac6bea3d26994109a11dd47
zong <zong-dev@protonmail.com>
2022-01-26 23:31:18 +0100

    bill payment cable subscription request

--- app/Http/Requests/Bill/Cable.php
+++ app/Http/Requests/Bill/Cable.php
@@ -13,7 +13,7 @@ class Cable extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,10 @@ class Cable extends FormRequest
     public function rules()
     {
         return [
-            //
+            'card_number' => 'required',
+            'provider' => 'required',
+            'variation_code' => 'required',
+            'pay_from' => 'required'
         ];
     }
 }
